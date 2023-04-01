c3141b5f35468d310bb79a094154c53c73806e9a
Aditya Gangil <111217346+adityagangil@users.noreply.github.com>
2023-04-01 23:06:52 +0530

    Add files via upload

---
+++ index2.js
@@ -0,0 +1,19 @@
+function handleSubmit(event)
+{
+    event.preventDefault();
+    console.log(event.target.username.value)
+    console.log(event.target.email.value)
+    console.log(event.target.phone.value)
+    console.log(event.target.date.value)
+    console.log(event.target.time.value)
+
+    let details = {
+        name: event.target.username.value,
+        email: event.target.email.value,
+        phone: event.target.phone.value,
+        date: event.target.date.value,
+        time: event.target.time.value,
+    }
+
+    localStorage.setItem('details', JSON.stringify(details))
+}
